aa6d17d94a5bf1523de3331f4614055659ac2837
Hanyang Chen <hchen26@gh-login03.delta.ncsa.illinois.edu>
2025-04-26 03:50:34 -0500

    123

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,8 +18,8 @@ pip install 'matplotlib'
 pip install 'flask'
 
 
-echo "Installing flash-attn with no build isolation..."
-pip install flash-attn --no-build-isolation
+# echo "Installing flash-attn with no build isolation..."
+# pip install flash-attn --no-build-isolation
 
 echo "Installing vagen package..."
 pip install -e .
@@ -28,8 +28,8 @@ echo "Installing Sokoban dependencies"
 pip install 'gym'
 pip install 'gym-sokoban'
 
-echo "Installing Frozenlake dependencies"
-pip install 'gymnasium'
-pip install "gymnasium[toy-text]"
+# echo "Installing Frozenlake dependencies"
+# pip install 'gymnasium'
+# pip install "gymnasium[toy-text]"
 
 echo "Installation complete, to install dependencies for other environments, refer to env/readme!"
