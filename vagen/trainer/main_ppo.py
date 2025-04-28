d4ef872de7c01fae5560bc096e55b02236f47af5
AquaHorseM <qinweimartin@gmail.com>
2025-04-28 05:48:34 +0000

    debugging

--- vagen/trainer/main_ppo.py
+++ vagen/trainer/main_ppo.py
@@ -29,7 +29,9 @@ def main(config):
 def run_ppo(config, compute_score=None):
     if not ray.is_initialized():
         # this is for local ray cluster
-        ray.init(runtime_env={'env_vars': {'TOKENIZERS_PARALLELISM': 'true', 'NCCL_DEBUG': 'DEBUG'}})
+        ray.init(runtime_env={'env_vars': {'TOKENIZERS_PARALLELISM': 'true', 'NCCL_DEBUG': 'WARN'}})
+    print("[DEBUG] ray initialized")
+    print(ray.cluster_resources())
 
     ray.get(main_task.remote(config, compute_score))
 
